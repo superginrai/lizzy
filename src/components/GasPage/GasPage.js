c2c976538426d59b2605e21568eccc01d9d79c98
Cory Booth <Rodimus2005@hotmail.com>
2018-12-28 11:45:15 -0600

    simple trips calculated

--- src/components/GasPage/GasPage.js
+++ src/components/GasPage/GasPage.js
@@ -7,10 +7,11 @@ class GasPage extends Component {
 
         this.state = {
             gasPrice: [],
+            tripCost: [],
         };
     }
 
-    ApiCall = () => {
+    apiCall = () => {
         axios({
             url: "http://api.eia.gov/series/?api_key=1f954176e259b0572af6796cad68fad0&series_id=PET.EMM_EPMRU_PTE_SMN_DPG.W",
             method: 'GET',
@@ -28,6 +29,31 @@ class GasPage extends Component {
             });
     };
 
+    valleyFair = () => {
+        let cost = (this.state.gasPrice * .33).toFixed(2);
+        this.setState({
+            tripCost: cost,
+        });
+    }
+
+    job1 = () => {
+        let cost = (this.state.gasPrice * .33).toFixed(2);
+        this.setState({
+            tripCost: cost,
+        });
+    }
+
+    job2 = () => {
+        let cost = (this.state.gasPrice * .067).toFixed(2);
+        this.setState({
+            tripCost: cost,
+        });
+    }
+
+    componentDidMount() {
+        // this.props.dispatch(fetchUser());
+        this.apiCall();
+    }
 
     render() {
         return (
@@ -36,8 +62,12 @@ class GasPage extends Component {
                     <p>
                         This page is about gas!!
       </p>
-                    <button onClick={this.ApiCall}>Gasszz </button>
                     Gas costs about ${this.state.gasPrice} per gallon!
+                    <p>Where are you going Lizzy?</p>
+                    <button onClick={this.job1}>Job 1</button>
+                    <button onClick={this.job2}>Job 2</button>
+                    <button onClick={this.valleyFair}>Valley Fair</button>
+                    <p>That trip will cost ${this.state.tripCost}.</p>
                 </div>
             </div>
         );
